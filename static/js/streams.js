224990385a142741b3f3f43c55c9854d2e95adb8
unknown <zmaktr@gmail.com>
2022-10-15 17:15:19 +0500

    configures agora

---
+++ static/js/streams.js
@@ -0,0 +1,5 @@
+// agora credentials
+const APP_ID = "bbab7cc117bf44ffbcf7198f3b57e889";
+const CHANNEL = "main";
+const TOKEN =
+  "007eJxTYNDzCstzyzgsX7Srzi0g2p4l7InwJquFs/kahP8eXS/17o0CQ1JSYpJ5crKhoXlSmolJWlpScpq5oaVFmnGSqXmqhYXlxvleyfWBjAwzqxcwMEIhiM/CkJuYmcfAAADtVR74";
